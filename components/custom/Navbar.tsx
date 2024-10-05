aa25691fdc4bb8599cc0c5766100090c816fd3b3
Rushikekhs Shrimanwar <100373043+rushikeshshirmanwar90@users.noreply.github.com>
2024-10-06 02:22:36 +0530

    initial commit

---
+++ components/custom/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+// shadcn components
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+// - Todo Dialog
+import TodoDialog from "@/components/custom/TodoDialog";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="shadow-2xl h-[5rem]">
+        <div className="flex justify-between items-center h-full mx-4 text-3xl font-semibold">
+          <div>TODO LIST</div>
+          <div>
+            <TodoDialog />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ components/custom/TodoDialog.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+// shadcn components
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const TodoDialog = () => {
+  return (
+    <>
+      <Dialog>
+        <DialogTrigger className="text-xl p-3 rounded-xl border border-black">
+          Add Todo
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle className="text-2xl font-semibold">
+              Add Todo List
+            </DialogTitle>
+            <DialogDescription className="text-sm ">
+              Add The Title And Description About the Todos
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Title
+              </Label>
+              <Input
+                id="name"
+                placeholder="Enter Todo Title.."
+                className="col-span-3"
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="description" className="text-right">
+                Description
+              </Label>
+              <Input
+                id="description"
+                placeholder="Enter The Description"
+                className="col-span-3"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default TodoDialog;
